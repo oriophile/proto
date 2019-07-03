cd28b375fa870f8b018200a87ab412abb3429fce
oriophile <bhagwati@incogtech.com>
2019-07-03 14:18:52 +0000

    2 wysiwyg editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -4,32 +4,7 @@ window.onload = function() {
 
 const editor = (function() {
     const editorClass = "incogtech_wysiwyg_editor";
-    const init = () => {
-        document.body.addEventListener("click", function(e) {
-            e.preventDefault();
-
-            //Remove all editors
-            let oldTarget = document.querySelectorAll("." + editorClass);
-            oldTarget.forEach(function(node) {
-                node.classList.remove(editorClass);
-                node.removeAttribute("contenteditable");
-            });
-
-            //Add editor on clicked TEXT NODE
-            let target = e.target;
-            target.click();
-            target.childNodes.forEach(function(cn) {
-                if(cn.nodeType == Node.TEXT_NODE) {
-                    target.classList.add(editorClass);
-                    target.setAttribute("contenteditable", true);
-                }
-            });
-
-            return false;
-        });
-        console.log("Click on body attached");
-
-        //Prevent from accidental navigation while deleting text (using backspace)
+    const disableBackspaceNavigation = function() {
         document.body.onkeydown = function(e) {
             let code = e.keyCode;
             if(((code == 8) || (code == 46)) && (e.target.nodeName == "BODY")) {
@@ -38,6 +13,46 @@ const editor = (function() {
             }
         }
         console.log("Disabled navigation from backspace to prevent accidental navigation.");
+    }
+
+    const removeEditorsExcept = function(target) {
+        let oldTarget = document.querySelectorAll("." + editorClass);
+        oldTarget.forEach(function(node) {
+            if(!node.isSameNode(target)) {
+                let txtNode = document.createTextNode(node.innerHTML);
+                node.parentNode.replaceChild(txtNode, node);
+            }
+        });
+    }
+
+    const addEditor = function(target) {
+        target.childNodes.forEach(function(cn) {
+            if((cn.nodeType == Node.TEXT_NODE) && (target.nodeName != "IT-WRAPPER") && (cn.nodeValue != null) && (cn.nodeValue.trim() != "")) {
+                console.log(encodeURIComponent(cn.nodeValue));
+                let wrap = document.createElement("it-wrapper");
+                wrap.appendChild(document.createTextNode(cn.nodeValue));
+                wrap.classList.add(editorClass);
+                wrap.setAttribute("contenteditable", true);
+                target.replaceChild(wrap, cn);
+            }
+        });
+    }
+
+    const init = () => {
+        document.body.addEventListener("mouseover", function(e) {
+            e.preventDefault();
+            let target = e.target;
+
+            removeEditorsExcept(target);
+
+            addEditor(target);
+
+            return false;
+        });
+        console.log("Click on body attached");
+
+        //Prevent from accidental navigation while deleting text (using backspace)
+        disableBackspaceNavigation();
     }
     return {
         init
